ef4ebf140fcdbfcc3d9400bd672acaa6720f9680
Nam <vannamtran@gmail.com>
2020-04-29 08:21:30 +0700

    Updates

---
+++ precache-manifest.0a9946c49d1f3749ea6bd186dd9ed541.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51d766625f3f49e9b8bf213c81428994",
+    "url": "/covid19report/index.html"
+  },
+  {
+    "revision": "0ae464eb74f19df5b78a",
+    "url": "/covid19report/static/css/main.467e994b.chunk.css"
+  },
+  {
+    "revision": "6cc176445551dfbcf1c0",
+    "url": "/covid19report/static/js/2.6086296c.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19report/static/js/2.6086296c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ae464eb74f19df5b78a",
+    "url": "/covid19report/static/js/main.a7120262.chunk.js"
+  },
+  {
+    "revision": "2525793a8ad1b603f22e",
+    "url": "/covid19report/static/js/runtime-main.aaf24a6a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid19report/static/media/image.d7265326.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19report/static/media/logo.5d5d9eef.svg"
+  }
+]);
